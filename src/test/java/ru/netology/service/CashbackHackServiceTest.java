a54152f2c225ffbd228355ec9335934b5a5a2502
Yuliya Gerashchenko <merg@list.ru>
2019-12-11 23:01:32 +0300

    initial commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,27 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    @Test
+    void calculateCashback() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 900;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 100;
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void noCalculateCashback() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 100;
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
